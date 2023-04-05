e4ed66a33d2c41f2e6c810f3a430f87d0259ec5a
LewisHaZ <feldman.lewis@Gmail.com>
2023-04-05 05:51:18 +0000

    add imports to validation file (feature)

---
+++ validation.py
@@ -0,0 +1,6 @@
+# Imports for the project requirements
+import time
+import gspread
+import os
+from google.oauth2.service_account import Credentials
+from email_validator import validate_email, EmailNotValidError
